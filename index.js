28d77337bac13a09bacfea17499bb2e260c26223
Joshua Fletcher <joshua.f1998@gmail.com>
2021-05-31 23:41:42 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function wakeDog(dogName, dogBreed) {
+  console.log("Wake ${dogName} the ${dogBreed}");
+}
+
+function leashDog(dogName, dogBreed) {
+  console.log("Leash ${dogName} the ${dogBreed}");
+}
+
+function walkDog(dogName, dogBreed) {
+  console.log("Walk to the park with ${dogName} the ${dogBreed}");
+}
+
+function frisbeeDog(dogName, dogBreed) {
+  console.log("Throw the frisbee for ${dogName} the ${dogBreed}");
+}
+
+function returnDog(dogName, dogBreed) {
+  console.log("Walk home with ${dogName} the ${dogBreed}");
+}
+
+function unleashDog(dogName, dogBreed) {
+  console.log("Unleash ${dogName}n the p${dogBreed}");
+}
